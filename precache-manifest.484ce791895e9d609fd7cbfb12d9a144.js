df99923604b0492d9bd40a6fe9c464d08c616fa6
s7illen <s7illen@gmail.com>
2019-11-28 11:06:34 -0500

    Updates

---
+++ precache-manifest.484ce791895e9d609fd7cbfb12d9a144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90927f1bc325068555f89e7a638f3ae",
+    "url": "/gh-pages-react-app/index.html"
+  },
+  {
+    "revision": "d4b2c3763f84818bec8e",
+    "url": "/gh-pages-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "566977cd107981fa4003",
+    "url": "/gh-pages-react-app/static/js/2.f0e4f202.chunk.js"
+  },
+  {
+    "revision": "d4b2c3763f84818bec8e",
+    "url": "/gh-pages-react-app/static/js/main.cde5b891.chunk.js"
+  },
+  {
+    "revision": "1f66ab8657c2648256e9",
+    "url": "/gh-pages-react-app/static/js/runtime-main.a2a3dedc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gh-pages-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
